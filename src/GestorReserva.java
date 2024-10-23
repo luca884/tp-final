8f7cd5e9eb8ed8d6e2036155807f53227c5f7ed3
Rodrigo Chavez <rodrigo@chavez.com>
2024-10-23 16:39:27 -0300

    Crea gestor reserva

---
+++ src/GestorReserva.java
@@ -0,0 +1,13 @@
+public class GestorReserva extends Gestor<Reserva> {
+    /*
+    @Override
+    public Reserva buscar(Integer id) {
+        for(Reserva reserva: lista){
+            if(reserva.getId() == id){
+                return reserva;
+            }
+        }
+        return null;
+    }
+     */
+}
