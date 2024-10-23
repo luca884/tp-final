b59c2dca8a69e12979b8dcfcbabff3a69717ee5f
luca <lucamarinio20@gmail.com>
2024-10-23 20:17:29 -0300

    Creo el paquete para la interfacez

---
+++ src/InterfazGrafica/IniciarSesion.java
@@ -0,0 +1,11 @@
+package InterfazGrafica;
+
+import javax.swing.*;
+
+public class IniciarSesion {
+    private JPanel panel1;
+    private JButton iniciarSesionButton;
+    private JButton registrarseButton;
+    private JTextField textField2;
+    private JPasswordField ingreseSuContraseñaPasswordField;
+}
---
+++ src/InterfazGrafica/Registrarse.java
@@ -0,0 +1,8 @@
+package InterfazGrafica;
+
+import javax.swing.*;
+
+public class Registrarse extends JFrame {
+
+    private JPanel panel1;
+}
