5ffa5037a342b121a6978150026321fe064da7ef
Zekencio <eze_reding@outlook.com>
2024-10-23 15:24:07 -0300

    Añadida la clase abstracta Servicio y el enum Estado

--- src/Empleado.java
+++ src/Empleado.java
@@ -1,6 +1,6 @@
 public class Empleado extends Persona {
 
-    protected Empleado(String nombre, String apellido, String dni) {
+    public Empleado(String nombre, String apellido, String dni) {
         super(nombre, apellido, dni);
     }
 }
