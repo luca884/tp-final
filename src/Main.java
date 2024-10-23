e0c4fc7ff60d1acf0bbf3af7a86452d70829bdc5
Zekencio <eze_reding@outlook.com>
2024-10-23 15:04:43 -0300

    coomit de prueba

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hoooolaaaaa");
 
+        System.out.println("asfasdfgvbsdvbjk");
 
         System.out.printf("mundddooo");
 
