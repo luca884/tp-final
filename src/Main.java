0519f2629c22eb89338130ea80988aa3ee7ae0a2
Rodrigo Chavez <rodrigo@chavez.com>
2024-10-31 14:10:27 -0300

    Agrega codigo de prueba comentado al main

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,26 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        /* GestorEmpleados gestor_empleados = new GestorEmpleados(); */
+        /*
+        HashSet<Empleado> empleados = new HashSet<>(List.of(
+                new Empleado("Pepe", "Sanchez", "12", 12.0, 8, Puesto.ADMINISTRADOR),
+                new Empleado("Maria", "Gomez", "34", 15.5, 9, Puesto.MANTENIMIENTO),
+                new Empleado("Luis", "Torres", "56", 10.0, 6, Puesto.SERVICIO_AL_CLIENTE),
+                new Empleado("Ana", "Martinez", "78", 20.0, 7, Puesto.ADMINISTRADOR)
+        ));
 
+        gestor_empleados.setLista(empleados);
+        gestor_empleados.guardarEnArchivo("empleados.json");
+         */
 
+        /*
+        gestor_empleados.cargarDesdeArchivo("empleados.json");
+        gestor_empleados.mostrar();
+        */
 
     }
 }
