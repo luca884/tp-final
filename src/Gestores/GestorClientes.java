2b21df0c7b86e8f7bb98e46c43192a3a080b55ec
Rodrigo Chavez <rodrigo@chavez.com>
2024-11-20 16:28:33 -0300

    Agrega metodo buscar por usuario

--- src/Gestores/GestorClientes.java
+++ src/Gestores/GestorClientes.java
@@ -1,8 +1,20 @@
 package Gestores;
 
 import Personas.Cliente;
+import Personas.Persona;
 
 public class GestorClientes extends Gestor<Cliente> {
+
+    /* metodos */
+    public Cliente buscarPorUsuario(String usuario){
+        for(Cliente cliente: getLista()){
+            if(cliente.getCredenciales().keySet().toArray()[0].toString().equals(usuario)){
+                return cliente;
+            }
+        }
+        return null;
+    }
+
     /* archivos json */
     public void cargarDesdeArchivo(String nombre_archivo) {
         super.cargarDesdeArchivo(nombre_archivo, Cliente.class);
