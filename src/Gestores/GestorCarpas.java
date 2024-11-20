5982f28d2b58cfec7471966d12ea3ba7288cee58
Rodrigo Chavez <rodrigo@chavez.com>
2024-11-20 20:37:15 -0300

    Agrega metodo para buscar reservables por tipo

--- src/Gestores/GestorCarpas.java
+++ src/Gestores/GestorCarpas.java
@@ -3,6 +3,16 @@ package Gestores;
 import Servicios.Carpa;
 
 public class GestorCarpas extends Gestor<Carpa> {
+    /* metodos */
+    public Carpa buscarReservablePorTipo(Enumeraciones.Carpa tipo_de_carpa) {
+        for (Carpa carpa : getLista()) {
+            if (carpa.esReservable() && carpa.getTipo_de_carpa() == tipo_de_carpa) {
+                return carpa;
+            }
+        }
+        return null;
+    }
+
     /* archivos json */
     public void cargarDesdeArchivo(String nombre_archivo) {
         super.cargarDesdeArchivo(nombre_archivo, Carpa.class);
